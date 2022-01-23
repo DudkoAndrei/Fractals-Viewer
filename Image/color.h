c7373ae30b4d8bd54e8b8af683eb05a5dd8c9981
niki4smirn <kitos.smirnov@gmail.com>
2022-01-23 18:15:01 +0300

    Color class added

---
+++ Image/color.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cstdint>
+
+class Color {
+ public:
+  Color() = default;
+  Color(uint8_t red, uint8_t green, uint8_t blue);
+
+  uint8_t r() const;
+  uint8_t g() const;
+  uint8_t b() const;
+
+  uint8_t& r();
+  uint8_t& g();
+  uint8_t& b();
+
+  Color operator*(double rhs) const;
+  Color operator+(const Color& rhs) const;
+
+ private:
+  uint8_t red_{0};
+  uint8_t green_{0};
+  uint8_t blue_{0};
+};
+
