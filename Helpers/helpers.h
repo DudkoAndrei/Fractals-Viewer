8bd3a4b273cb7f9761d4da991e91b1aab5b16adb
niki4smirn <kitos.smirnov@gmail.com>
2022-01-25 20:07:34 +0300

    MapValue hotfix

--- Helpers/helpers.h
+++ Helpers/helpers.h
@@ -3,12 +3,16 @@
 namespace helpers {
 #include "constants.h"
 
-template<typename T1, typename T2>
-T2 MapValue(T1 value, T1 value_min, T1 value_max, T2 map_min, T2 map_max) {
+template<typename T>
+T MapValue(long double value,
+           long double value_min,
+           long double value_max,
+           T map_min,
+           T map_max) {
   value = std::max(std::min(value, value_max), value_min);
   if (value_max == value_min)
     return map_max;
-  return (value - value_min) * (map_max - map_min) / (value_max - value_min) +
+  return (value - value_min) / (value_max - value_min) * (map_max - map_min) +
       map_min;
 }
 }  // namespace helpers
