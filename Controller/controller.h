5589de3dc22023a07679f005c6154d847d17b010
DudkoAndrei <dudkoandrei@gmail.com>
2022-01-26 16:21:09 +0300

    feat(Controller): Add RunCudaTest method

--- Controller/controller.h
+++ Controller/controller.h
@@ -2,14 +2,17 @@
 
 #include <memory>
 
+#include "../Cuda/cuda_formula_image_processor.cuh"
 #include "../ImageProcessor/formula_image_processor.h"
 
 class Controller {
  public:
   void RunTest();
+  void RunCudaTest();
 
  private:
   std::unique_ptr<FormulaImageProcessor> image_processor_;
+  std::unique_ptr<CudaFormulaImageProcessor> cuda_image_processor_;
 };
 
 
