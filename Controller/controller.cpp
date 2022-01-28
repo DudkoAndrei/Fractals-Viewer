1876286435cb388d58b33bb000e5098878ff06c3
DudkoAndrei <dudkoandrei@gmail.com>
2022-01-28 21:06:45 +0300

    refactor: Remove CudaFormulaImageProcessor

--- Controller/controller.cpp
+++ Controller/controller.cpp
@@ -21,12 +21,12 @@ void Controller::RunTest() {
   image_processor_ = std::make_unique<FormulaImageProcessor>();
 
   ImageSettings settings = {1000, 1000, 0, 0, 1, 1};
-  image_processor_->GenerateImage(settings).WriteToFile("first.png");
+  image_processor_->GenerateImage(false,settings).WriteToFile("first.png");
 }
 
 void Controller::RunCudaTest() {
-  cuda_image_processor_ = std::make_unique<CudaFormulaImageProcessor>();
+  image_processor_ = std::make_unique<FormulaImageProcessor>();
 
   ImageSettings settings = {1000, 1000, 0, 0, 200, 200};
-  cuda_image_processor_->GenerateImage(settings).WriteToFile("cuda_test.png");
+  image_processor_->GenerateImage(true,settings).WriteToFile("cuda_test.png");
 }
