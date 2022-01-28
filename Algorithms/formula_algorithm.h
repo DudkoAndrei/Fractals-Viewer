2b0b1766e6e67812dd5645151a1b8ca39bd7dfda
niki4smirn <kitos.smirnov@gmail.com>
2022-01-28 21:10:12 +0300

    Gradient added to ImageProcessor

--- Algorithms/formula_algorithm.h
+++ Algorithms/formula_algorithm.h
@@ -5,18 +5,18 @@
 #include <utility>
 
 #include "abstract_formula_algorithm.h"
-#include "../Point/point.h"
+#include "../Point/point_info.h"
 #include "../Cuda/PolynomialCalculator/polynomial_calculator.cuh"
 
 class FormulaAlgorithm : public AbstractFormulaAlgorithm {
  public:
   void Calculate(
-      std::vector<uint64_t>* iters_count,
+      std::vector<PointInfo>* iters_count,
       const ImageSettings& settings,
       const std::vector<Token>& expression) const override;
 
  private:
-  uint64_t CalculatePoint(
+  static PointInfo CalculatePoint(
       const Point& point,
-      const PolynomialCalculator<double>& calc) const;
+      const PolynomialCalculator<double>& calc);
 };
--- Cuda/cuda_formula_algorithm.cu
+++ Cuda/cuda_formula_algorithm.cu
@@ -2,8 +2,8 @@
 #include "fractal_algorithms.cuh"
 
 void CudaFormulaAlgorithm::Calculate(
-    std::vector<uint64_t>* iters_count,
+    std::vector<PointInfo>* point_info,
     const ImageSettings& settings,
     const std::vector<Token>& expression) const {
-  CudaBWFractal(iters_count, settings, expression);
+  CudaBWFractal(point_info, settings, expression);
 }
