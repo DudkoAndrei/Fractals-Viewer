8a0cc0bb68f1773f71a6a67d86edb34ba65c00cd
DudkoAndrei <dudkoandrei@gmail.com>
2022-01-28 20:55:33 +0300

    refactor(CalculatePoint): Replace std::complex with own class

--- Algorithms/formula_algorithm.cpp
+++ Algorithms/formula_algorithm.cpp
@@ -1,12 +1,11 @@
 #include "formula_algorithm.h"
-
-#include <complex>
+#include "../Cuda/complex.cuh"
 
 void FormulaAlgorithm::Calculate(
     std::vector<uint64_t>* iters_count,
     const ImageSettings& settings,
     const std::vector<Token>& expression) const {
-  PolynomialCalculator<double> calc(expression);
+  PolynomialCalculator<double> calc(expression.data(), expression.size());
   for (int y = 0; y < settings.height; ++y) {
     for (int x = 0; x < settings.width; ++x) {
       (*iters_count)[y * settings.width + x] = CalculatePoint(Point(x, y),
@@ -19,12 +18,12 @@ void FormulaAlgorithm::Calculate(
 uint64_t FormulaAlgorithm::CalculatePoint(
     const Point& point,
     const PolynomialCalculator<double>& calc) const {
-  std::complex<double> c((point.x() - 500) / 200.0,
+  Complex<double> c((point.x() - 500) / 200.0,
                          (point.y() - 500) / 200.0);
-  std::complex<double> z(0.0, 0.0);
+  Complex<double> z(0.0, 0.0);
 
   size_t iteration = 0;
-  while (iteration < 1000 && abs(z) < (2 << 8)) {
+  while (iteration < 1000 && z.Abs() < (2 << 8)) {
     z = calc.Calculate(z, c);
     ++iteration;
   }
