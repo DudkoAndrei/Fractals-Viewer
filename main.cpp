053a64a266e6b9964b33e3753924370560e718fe
niki4smirn <kitos.smirnov@gmail.com>
2022-01-31 17:08:26 +0300

    TimeMeasurer class added

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
+#include <iostream>
+
 #include "Controller/controller.h"
+#include "Helpers/TimeMeasurer/time_measurer.h"
 
 int main() {
   Controller controller;
 
+  TimeMeasurer default_run_time;
   controller.RunTest();
+  std::cerr << "Default Run Time: ";
+  std::cerr << default_run_time.GetElapsedSeconds() << "s\n";
+  TimeMeasurer cuda_run_time;
   controller.RunCudaTest();
+  std::cerr << "Cuda Run Time: ";
+  std::cerr << cuda_run_time.GetElapsedSeconds() << "s\n";
   return 0;
 }
