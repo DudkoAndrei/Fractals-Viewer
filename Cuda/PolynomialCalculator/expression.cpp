726170dbc4ab0c0c64db8667b7f0bc676ef4e5bf
niki4smirn <kitos.smirnov@gmail.com>
2022-02-05 20:31:44 +0300

    Fixed UB in memcpy call

--- Cuda/PolynomialCalculator/expression.cpp
+++ Cuda/PolynomialCalculator/expression.cpp
@@ -16,23 +16,31 @@ Expression::Expression(
   data_ = new double[new_size];
 
   segments_.default_segment = {start_pos, default_coefs.size()};
-  memcpy(data_ + start_pos, default_coefs.data(),
-         default_coefs.size() * sizeof(double));
+  if (!default_coefs.empty()) {
+    memcpy(data_ + start_pos, default_coefs.data(),
+           default_coefs.size() * sizeof(double));
+  }
   start_pos += default_coefs.size();
 
   segments_.conjugate_segment = {start_pos, conjugate_coefs.size()};
-  memcpy(data_ + start_pos, conjugate_coefs.data(),
-         conjugate_coefs.size() * sizeof(double));
+  if (!conjugate_coefs.empty()) {
+    memcpy(data_ + start_pos, conjugate_coefs.data(),
+           conjugate_coefs.size() * sizeof(double));
+  }
   start_pos += conjugate_coefs.size();
 
   segments_.transpose_segment = {start_pos, transpose_coefs.size()};
-  memcpy(data_ + start_pos, transpose_coefs.data(),
-         transpose_coefs.size() * sizeof(double));
+  if (!transpose_coefs.empty()) {
+    memcpy(data_ + start_pos, transpose_coefs.data(),
+           transpose_coefs.size() * sizeof(double));
+  }
   start_pos += transpose_coefs.size();
 
   segments_.absolute_segment = {start_pos, absolute_coefs.size()};
-  memcpy(data_ + start_pos, absolute_coefs.data(),
-         absolute_coefs.size() * sizeof(double));
+  if (!absolute_coefs.empty()) {
+    memcpy(data_ + start_pos, absolute_coefs.data(),
+           absolute_coefs.size() * sizeof(double));
+  }
   start_pos += absolute_coefs.size();
 }
 
